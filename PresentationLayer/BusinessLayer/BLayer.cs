50c5625704df4f2b7a2bf28d820d9fd0c44ed493
Nenad-PC\Nenad <nenad.penezi264@gmail.com>
2020-12-21 08:48:29 -0800

    BusinessLayer is created.

---
+++ PresentationLayer/BusinessLayer/BLayer.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer;
+using System;
+
+namespace BusinessLayer
+{
+    public class BLayer
+    {
+        public readonly PlayerRepository pr;
+
+        public BLayer()
+        {
+            pr = new PlayerRepository();
+        }
+    }
+}
