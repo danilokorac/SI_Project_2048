c491c695db51dad92b5470c74d36a489e504e1cf
tomicnikola <50-2018@ftn.edu.rs>
2020-12-21 16:01:47 +0100

    Connected database with the application

---
+++ PresentationLayer/DataAccessLayer/Models/PersonalScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class PersonalScore
+    {
+        public int PlayerID { get; set; }
+        public int Score { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int NumberOfMoves { get; set; }
+        public DateTime TimePlayed { get; set; }
+    }
+}
